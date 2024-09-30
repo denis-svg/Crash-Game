8bc1ca5d1c5a6fbb437bd1b4fb69608d8064ee14
deniska <dprodan289@gmail.com>
2024-09-30 13:29:28 +0300

    fixed database bugs user service

--- auth/config.py
+++ auth/config.py
@@ -1,4 +1,4 @@
 class Config:
     SECRET_KEY = "test"
-    SQLALCHEMY_DATABASE_URI = "mysql://root:1111@localhost/auth_db"
+    SQLALCHEMY_DATABASE_URI = "mysql://root:1111@mysql/auth_db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
--- auth/db_init.py
+++ auth/db_init.py
@@ -1,5 +1,19 @@
 from app import app, db
+import MySQLdb
+
+# Function to create the database if it doesn't exist
+def create_database_if_not_exists():
+    db = MySQLdb.connect(host="mysql", user="root", passwd="1111")
+
+    c = db.cursor()
+    c.execute(f"CREATE DATABASE IF NOT EXISTS auth_db")
+
+    db.close()
 
 with app.app_context():
-    db.create_all()
-    print("Database initialized")
+    try:
+        create_database_if_not_exists()
+        db.create_all()  # Create tables if they don't exist
+        print("Database initialized")
+    except Exception as e:
+        print(f"Error initializing database: {e}")
